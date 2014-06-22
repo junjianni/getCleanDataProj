1a4ed987a02e3b38aa8f709a5c2896a7f619caae
junjianni <junjianni@yahoo.com>
2014-06-21 22:04:29 -0400

    Update run_analysis.R
    Edit format.

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
-	## R code for "Getting and Cleaning Data" course project from Coursea
+  ## R code for "Getting and Cleaning Data" course project from Coursea
   # run_analysis.R does following steps:  
-  #1.	Merges the training and the test sets to create one data set.
+        #1.	Merges the training and the test sets to create one data set.
 	#2.	Extracts only the measurements on the mean and standard #deviation for each measurement.
 	#3.	Uses descriptive activity names to name the activities in the data #set
 	#4.	Appropriately labels the data set with descriptive activity names.
-  #5.	Creates a second, independent tidy data set with the average of #each variable for each activity and each subject.
+        #5.	Creates a second, independent tidy data set with the average of #each variable for each activity and each subject.
 
 # pre-step:
 # load required library:
